5b077f3cdbf9708907eb21c0be027c57b1839eb2
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2021-09-01 18:00:07 -0700

    Added menu scene, pause menu, and started menu UI

---
+++ Cursed Crops/Assets/Scripts/Pause_Manager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Manager : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+    public GameObject PauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Unpause();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Pause()
+    {
+        gamePaused = true;
+        Time.timeScale = 0f;
+        PauseMenu.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        gamePaused = false;
+        Time.timeScale = 1f;
+        PauseMenu.SetActive(false);
+    }
+
+    public void ExitToMenu()
+    {
+        Unpause();
+        // SceneManager.LoadScene("MenuScene");
+        SceneManager.LoadScene(0);
+    }
+}
