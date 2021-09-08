6ebf962d83c9b4093b78943b9ef4b61631c837f0
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-09-08 15:19:49 -0700

    Enemies that chase player will change their attention to main objective when player is stun

--- Cursed Crops/Assets/Scripts/EnemyToPlayer.cs
+++ Cursed Crops/Assets/Scripts/EnemyToPlayer.cs
@@ -8,19 +8,34 @@ public class EnemyToPlayer : MonoBehaviour
     //Setting up changable variables for enemies speeds
     public float enemySpeed = 1f;
     public Transform Player;
+    public Transform mainTarget;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        mainTarget = GameObject.FindGameObjectWithTag("MainObjective").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 position = Vector3.MoveTowards(transform.position, Player.position, enemySpeed * Time.fixedDeltaTime);
-        rb.MovePosition(position);
-        transform.LookAt(Player);
+        if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControler>().enabled)
+        {
+            Vector3 position = Vector3.MoveTowards(transform.position, Player.position, enemySpeed * Time.fixedDeltaTime);
+            rb.MovePosition(position);
+            transform.LookAt(Player);
+        }
+        
+        else if (!GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControler>().enabled)
+        {
+            if (mainTarget != null)
+            {
+                Vector3 objectivePosition = Vector3.MoveTowards(transform.position, mainTarget.position, enemySpeed * Time.fixedDeltaTime);
+                rb.MovePosition(objectivePosition);
+                transform.LookAt(mainTarget);
+            }
+        }
     }
 }
