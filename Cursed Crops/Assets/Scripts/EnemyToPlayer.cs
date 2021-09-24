db864d94c6d26d27c806fff6f23bfa4655b5a7c1
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-09-24 15:45:24 -0700

    cleaned up enemytoplayer script

--- Cursed Crops/Assets/Scripts/EnemyToPlayer.cs
+++ Cursed Crops/Assets/Scripts/EnemyToPlayer.cs
@@ -19,14 +19,12 @@ public class EnemyToPlayer : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //Player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
         listOfPlayers = new Transform[players.Length];
         
         for (int i = 0; i < listOfPlayers.Length; ++i)
             listOfPlayers[i] = players[i].transform;
 
-        Debug.Log(players.Length);
         mainTarget = GameObject.FindGameObjectWithTag("MainObjective").GetComponent<Transform>();
     }
 
@@ -34,10 +32,7 @@ public class EnemyToPlayer : MonoBehaviour
     void Update()
     {
         // new multiplayer chase code
-        //allPlayersStun();
         Transform closestPlayer = FindClosestPlayer(listOfPlayers);
-        //if (!aPlayerIsStun)
-        //{
             
 
             Vector3 position = Vector3.MoveTowards(transform.position, closestPlayer.position, enemySpeed * Time.fixedDeltaTime);
@@ -47,39 +42,7 @@ public class EnemyToPlayer : MonoBehaviour
                 Vector3 objectivePosition = Vector3.MoveTowards(transform.position, mainTarget.position, enemySpeed * Time.fixedDeltaTime);
                 rb.MovePosition(objectivePosition);
             }
-        //}
-            
-        //else if (aPlayerIsStun)
-        //{
-        //    Vector3 objectivePosition = Vector3.MoveTowards(transform.position, mainTarget.position, enemySpeed * Time.fixedDeltaTime);
-        //    rb.MovePosition(objectivePosition);
-        //}
-            
-        //old one player chase code
-        /*
-        if (GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControler>().enabled)
-        {
-            Vector3 position = Vector3.MoveTowards(transform.position, Player.position, enemySpeed * Time.fixedDeltaTime);
-            rb.MovePosition(position);
-            if (targetChange)
-            {
-                Vector3 objectivePosition = Vector3.MoveTowards(transform.position, mainTarget.position, enemySpeed * Time.fixedDeltaTime);
-                rb.MovePosition(objectivePosition);
-            }
-            //transform.LookAt(Player);
-        }
-
-        else if (!GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControler>().enabled)
-        {
-            if (mainTarget != null)
-            {
-                //targetChange = true;
-                Vector3 objectivePosition = Vector3.MoveTowards(transform.position, mainTarget.position, enemySpeed * Time.fixedDeltaTime);
-                rb.MovePosition(objectivePosition);
-                //transform.LookAt(mainTarget);
-            }
-        }
-        */
+        
 
     }
 
