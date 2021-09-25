68c8dad2bf0567fc8a4990b1402ddaf8be02b16d
Jared Nolen <Legodoghead@gmail.com>
2021-09-24 17:07:12 -0700

    Redid input for keyboard/controller to use new input system. (Co-op not yet setup))

--- Cursed Crops/Assets/Scripts/EnemyToPlayer.cs
+++ Cursed Crops/Assets/Scripts/EnemyToPlayer.cs
@@ -63,6 +63,7 @@ public class EnemyToPlayer : MonoBehaviour
                     closestDistanceSqr = dSqrToTarget;
                     bestTarget = potentialTarget;
                 }
+                
             
         }
 
