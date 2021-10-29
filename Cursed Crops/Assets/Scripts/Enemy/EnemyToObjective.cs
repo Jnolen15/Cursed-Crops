173f9392a3754296c981e6de1d3736dfce715f69
KapeRay <67844957+KapeRay@users.noreply.github.com>
2021-10-29 15:26:30 -0700

    Added pathfinding algorithm

--- Cursed Crops/Assets/Scripts/Enemy/EnemyToObjective.cs
+++ Cursed Crops/Assets/Scripts/Enemy/EnemyToObjective.cs
@@ -8,20 +8,57 @@ public class EnemyToObjective : MonoBehaviour
     public float enemySpeed = 1f;
     public Transform Objective;
     Rigidbody rb;
+    Vector3[] path;
+    int targetIndex;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         Objective = GameObject.FindGameObjectWithTag("MainObjective").GetComponent<Transform>();
+        PathRequestManager.RequestPath(transform.position, Objective.position, OnPathFound);
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if(Objective != null){
-            Vector3 position = Vector3.MoveTowards(transform.position, Objective.position, enemySpeed * Time.fixedDeltaTime);
-            rb.MovePosition(position);
+            //Vector3 position = Vector3.MoveTowards(transform.position, Objective.position, enemySpeed * Time.fixedDeltaTime);
+            //rb.MovePosition(position);
             //transform.LookAt(Objective);
+                
+        }
+        
+    }
+
+    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
+    {
+        if (pathSuccessful)
+        {
+            path = newPath;
+            //StopCoroutine("FollowPath");
+            StartCoroutine("FollowPath");
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        Vector3 currentWaypoint = path[0];
+        while (true)
+        {
+            if(transform.position == currentWaypoint)
+            {
+                targetIndex++;
+                if(targetIndex >= path.Length)
+                {
+                    yield break;
+                }
+                currentWaypoint = path[targetIndex];
+            }
+            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, enemySpeed * Time.deltaTime);
+            //rb.MovePosition(position);
+            yield return null;
         }
     }
 }
