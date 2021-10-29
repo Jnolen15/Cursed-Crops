18bcc71e952664a5b449873d4a9b21d699de4119
Jared Nolen <Legodoghead@gmail.com>
2021-10-29 15:37:56 -0700

    Bigger plane and rebinding test

--- Cursed Crops/Assets/Scripts/PlayerManager.cs
+++ Cursed Crops/Assets/Scripts/PlayerManager.cs
@@ -7,6 +7,7 @@ public class PlayerManager : MonoBehaviour
 {
     public static List<GameObject> players = new List<GameObject>();
     public PlayerAnimOCManager animManager;
+    public RebindingDisplay rebinding;
 
     // Start is called before the first frame update
     void Start()
@@ -22,11 +23,18 @@ public class PlayerManager : MonoBehaviour
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
+        // When a player joins add them to the player array
         players.Add(playerInput.gameObject);
+
+        // Assing player correct animations
         animManager = playerInput.gameObject.GetComponent<PlayerAnimOCManager>();
         if (players.IndexOf(playerInput.gameObject) == 0) animManager.selectedCharacter = PlayerAnimOCManager.character.harvey;
         else if (players.IndexOf(playerInput.gameObject) == 1) animManager.selectedCharacter = PlayerAnimOCManager.character.doug;
 
+        // Rebinding
+        rebinding.playerControler = playerInput.gameObject.GetComponent<PlayerControler>();
+
+        // Debug
         foreach (var item in players)
         {
             Debug.Log(item.ToString());
