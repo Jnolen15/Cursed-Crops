35a1a9503c77c4919682d055fd549f7b0c4eb249
Jared Nolen <Legodoghead@gmail.com>
2021-10-29 15:55:07 -0700

    Revert "Bigger plane and rebinding test"
    This reverts commit 18bcc71e952664a5b449873d4a9b21d699de4119.

--- Cursed Crops/Assets/Scripts/Enemy/EnemyToPlayer.cs
+++ Cursed Crops/Assets/Scripts/Enemy/EnemyToPlayer.cs
@@ -14,9 +14,6 @@ public class EnemyToPlayer : MonoBehaviour
     private bool targetChange = false;
     public Transform[] listOfPlayers;
     public bool aPlayerIsStun = true;
-    Vector3[] path;
-    int targetIndex = 0;
-    private bool playerinbound = true;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +24,8 @@ public class EnemyToPlayer : MonoBehaviour
         
         for (int i = 0; i < listOfPlayers.Length; ++i)
             listOfPlayers[i] = players[i].transform;
-        
+
         mainTarget = GameObject.FindGameObjectWithTag("MainObjective").GetComponent<Transform>();
-        //Transform closestPlayer = FindClosestPlayer(listOfPlayers);
-        //PathRequestManager.RequestPath(transform.position, closestPlayer.position, OnPathFound);
-
-
     }
 
     // Update is called once per frame
@@ -40,16 +33,16 @@ public class EnemyToPlayer : MonoBehaviour
     {
         // new multiplayer chase code
         Transform closestPlayer = FindClosestPlayer(listOfPlayers);
-        PathRequestManager.RequestPath(transform.position, closestPlayer.position, OnPathFound);
-        /*
-        Vector3 position = Vector3.MoveTowards(transform.position, closestPlayer.position, enemySpeed * Time.fixedDeltaTime);
+            
+
+            Vector3 position = Vector3.MoveTowards(transform.position, closestPlayer.position, enemySpeed * Time.fixedDeltaTime);
             rb.MovePosition(position);
             if (targetChange)
             {
                 Vector3 objectivePosition = Vector3.MoveTowards(transform.position, mainTarget.position, enemySpeed * Time.fixedDeltaTime);
                 rb.MovePosition(objectivePosition);
             }
-        */
+        
 
     }
 
@@ -97,59 +90,6 @@ public class EnemyToPlayer : MonoBehaviour
         {
             targetChange = true;
 
-        }
-        if (other.gameObject.name == "Player")
-        {
-            playerinbound = false;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.name == "Player")
-        {
-            playerinbound = true;
-        }
-    }
-
-    public void OnPathFound(Vector3[] newPath, bool pathSuccessful)
-    {
-        if (pathSuccessful)
-        {
-            path = newPath;
-            if (path.Length > 0)
-            {
-                StopCoroutine("FollowPath");
-                StartCoroutine("FollowPath");
-            }
-        }
-    }
-
-
-
-    IEnumerator FollowPath()
-    {
-
-        Vector3 currentWaypoint = path[0];
-        int lengthOfCurrent = path.Length;
-        int checker = 0;
-        print(lengthOfCurrent);
-        while (true)
-        {
-            if (transform.position == currentWaypoint)
-            {
-                targetIndex++;
-                if (targetIndex >= path.Length)
-                {
-                    yield break;
-                }
-                currentWaypoint = path[targetIndex];
-            }
-            checker++;
-            //print(currentWaypoint);
-            transform.position = Vector3.MoveTowards(transform.position, currentWaypoint, enemySpeed * Time.deltaTime);
-            //rb.MovePosition(position);
-            yield return null;
         }
     }
 }
