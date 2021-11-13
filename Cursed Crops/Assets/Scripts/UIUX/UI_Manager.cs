e9fdd9e1d91fddb51a0dc4bd1e3cf134c906c8eb
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2021-11-12 16:14:10 -0800

    updated UI

--- Cursed Crops/Assets/Scripts/UIUX/UI_Manager.cs
+++ Cursed Crops/Assets/Scripts/UIUX/UI_Manager.cs
@@ -6,11 +6,18 @@ using TMPro;
 
 public class UI_Manager : MonoBehaviour
 {
+    private GameObject house;
+    private EnemyDamageObjective EDO;
+    private GameObject p1;
     private PlayerControler PC;
+    private EnemyPlayerDamage EPD;
     public GameObject p1UI;
     // UI elements must me dragged/droppped
+    public TextMeshProUGUI nameText;
     public TextMeshProUGUI currencyText;
+    public TextMeshProUGUI vegetableText;
     public Slider HealthBar;
+    public Slider HouseHealthBar;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +25,23 @@ public class UI_Manager : MonoBehaviour
         // aquiring player controller
         // dependant on player name, might need to be improved
         // PC = GameObject.Find("Player(Clone)").GetComponent<PlayerControler>();
+        house = GameObject.Find("Objective");
+        EDO = house.GetComponent<EnemyDamageObjective>();
     } 
 
     // Update is called once per frame
     void Update()
     {
-        if (PC == null && GameObject.Find("Player") != null)
+        if (PC == null && GameObject.Find("Player(Clone)") != null)
         {
-            PC = GameObject.Find("Player").GetComponent<PlayerControler>();
+            p1 = GameObject.Find("Player(Clone)");
+            PC = p1.GetComponent<PlayerControler>();
+            EPD = p1.GetComponent<EnemyPlayerDamage>();
+
             // also need to catch the error created by not finding a player initially
             p1UI.SetActive(true);
+            // players prefabs currenty don't store their own name as far as I can tell
+            nameText.text = "Harvey";
         }
 
         // checking if PC has been aquired
@@ -35,9 +49,13 @@ public class UI_Manager : MonoBehaviour
         {
             // managing  currency text
             currencyText.text = "Monies: " + PC.money;
+            vegetableText.text = "Monies: " + PC.money;
             // managing health bar
-            HealthBar.value = PC.health / PC.maxHealth;
+            HealthBar.value = EPD.playerHealth / EPD.reviveHealth;
         }
+
+        // updating general UI
+        HouseHealthBar.value = EDO.houseHealth / 50;
     }
 
     
