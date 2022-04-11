5de9ac27a52e39e2dffc40b7a6a2bed310a0ddd9
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-04-11 14:48:08 -0700

    Worked on CharSelect Scene
    - Made a seperate CharacterSelectManager
    - Made a CharacterSelectWheel for each player
    - made it so two players can't select the same character
    - added Character select scene to build index

--- Cursed Crops/Assets/Scripts/UIUX/CharacterSelectManager.cs
+++ Cursed Crops/Assets/Scripts/UIUX/CharacterSelectManager.cs
@@ -1,115 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
 public class CharacterSelectManager : MonoBehaviour
 {
-    // the visual objects that are being changed
-    public GameObject DougImage;
-    public GameObject CecilImage;
-    public GameObject HarveyImage;
-    public GameObject CarlisleImage;
-    public Button ScrollUpButton;
-    public Button ScrollDownButton;
+    // the character Select Wheels
+    public CharacterSelectWheel Player1;
+    public CharacterSelectWheel Player2;
+    public CharacterSelectWheel Player3;
+    public CharacterSelectWheel Player4;
 
-    public GameObject SelectionIndicator;
-    public TextMeshProUGUI NameText;
+    public List<string> AvailableChars = new List<string>();
 
-    // internal variables
-    public bool charSelected = false;
-    public GameObject previousChar;
-
-    // enum for tracking which Character is selected
-    public enum Character
-    {
-        // 0 - 3, respectively
-        Doug, 
-        Cecil,
-        Harvey, 
-        Carlisle
-    }
-
-    public Character character;
-
+    // internal 
 
     void Start()
     {
-        character = 0;
-        previousChar = DougImage;
+        // adding available characters to string
+        AvailableChars.Add("Doug");
+        AvailableChars.Add("Cecil");
+        AvailableChars.Add("Harvey");
+        AvailableChars.Add("Carlisle");
     }
 
-    public void ShowCharacter()
+    void Update()
     {
-        previousChar.SetActive(false);
-        NameText.text = character.ToString();
-
-        switch (character)
+        if (Player1.charSelected && Player2.charSelected && Player3.charSelected && Player4.charSelected)
         {
-            case Character.Doug:
-                previousChar = DougImage;
-                
-                break;
+            // store player selections
+            PlayerPrefs.SetString("Player1", Player1.character.ToString());
+            PlayerPrefs.SetString("Player2", Player2.character.ToString());
+            PlayerPrefs.SetString("Player3", Player3.character.ToString());
+            PlayerPrefs.SetString("Player4", Player4.character.ToString());
 
-            case Character.Cecil:
-                previousChar = CecilImage;
-                break;
-
-            case Character.Harvey:
-                previousChar = HarveyImage;
-                break;
-
-            case Character.Carlisle:
-                previousChar = CarlisleImage;
-                break;
+            StartLevel();
         }
-        previousChar.SetActive(true);
     }
 
-    // scroll up decreases char#
-    public void ScrollUp()
+    public void StartLevel()
     {
-        if (!charSelected)
-        {
-            // increment Character, or loop back to start
-            if (character > 0)
-            {
-                character -= 1;
-            } else
-            {
-                character += 3;
-            }
-        }
-        ShowCharacter();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // scroll down increases char#
-    public void ScrollDown()
+    public void Exit()
     {
-        if (!charSelected)
-        {
-            // increment Character, or loop back to start
-            // idk why but this won't accept 3 even though Character.Carlisle = 3
-            if (character < Character.Carlisle)
-            {
-                character += 1;
-            }
-            else
-            {
-                character -= 3;
-            }
-        }
-        ShowCharacter();
-    }
-
-    // toggles wether a character is selected or not
-    public void ToggleSelect()
-    {
-        charSelected = !charSelected;
-        SelectionIndicator.SetActive(!SelectionIndicator.activeSelf);
-        // disabling buttons
-        ScrollUpButton.interactable = !ScrollUpButton.interactable;
-        ScrollDownButton.interactable = !ScrollDownButton.interactable;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 }
---
+++ Cursed Crops/Assets/Scripts/UIUX/CharacterSelectWheel.cs
@@ -0,0 +1,143 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CharacterSelectWheel : MonoBehaviour
+{
+    // the visual objects that are being changed
+    public GameObject DougImage;
+    public GameObject CecilImage;
+    public GameObject HarveyImage;
+    public GameObject CarlisleImage;
+    public Button ScrollUpButton;
+    public Button ScrollDownButton;
+
+    public GameObject SelectionIndicator;
+    public TextMeshProUGUI NameText;
+
+    // internal variables
+    public bool charSelected = false;
+    public GameObject previousChar;
+
+    // External Variables
+    public CharacterSelectManager CharacterSelectManager;
+
+    // enum for tracking which Character is selected
+    public enum Character
+    {
+        // 0 - 3, respectively
+        Doug, 
+        Cecil,
+        Harvey, 
+        Carlisle
+    }
+
+    public Character character;
+
+
+    void Start()
+    {
+        CharacterSelectManager = this.GetComponentInParent<CharacterSelectManager>();
+        character = 0;
+        previousChar = DougImage;
+    }
+
+    public void ShowCharacter()
+    {
+        previousChar.SetActive(false);
+        NameText.text = character.ToString();
+
+        switch (character)
+        {
+            case Character.Doug:
+                previousChar = DougImage;
+                
+                break;
+
+            case Character.Cecil:
+                previousChar = CecilImage;
+                break;
+
+            case Character.Harvey:
+                previousChar = HarveyImage;
+                break;
+
+            case Character.Carlisle:
+                previousChar = CarlisleImage;
+                break;
+        }
+        previousChar.SetActive(true);
+    }
+
+    // scroll up decreases char#
+    public void ScrollUp()
+    {
+        if (!charSelected)
+        {
+            // increment Character, or loop back to start
+            if (character > 0)
+            {
+                character -= 1;
+            } else
+            {
+                character += 3;
+            }
+        }
+        ShowCharacter();
+    }
+
+    // scroll down increases char#
+    public void ScrollDown()
+    {
+        if (!charSelected)
+        {
+            // increment Character, or loop back to start
+            // idk why but this won't accept 3 even though Character.Carlisle = 3
+            if (character < Character.Carlisle)
+            {
+                character += 1;
+            }
+            else
+            {
+                character -= 3;
+            }
+        }
+        ShowCharacter();
+    }
+
+    // toggles wether a character is selected or not
+    public void ToggleSelect()
+    {
+        charSelected = !charSelected;
+        SelectionIndicator.SetActive(!SelectionIndicator.activeSelf);
+        // disabling buttons
+        ScrollUpButton.interactable = !ScrollUpButton.interactable;
+        ScrollDownButton.interactable = !ScrollDownButton.interactable;
+    }
+
+    public void SelectCharacter()
+    {
+        if (charSelected)
+        {
+            // re-add character to availableChars
+            CharacterSelectManager.AvailableChars.Add(character.ToString());
+            ToggleSelect();
+        }
+        else
+        {
+            // check to see if char is available, then remove from list
+            if (CharacterSelectManager.AvailableChars.Contains(character.ToString()))
+            {
+                CharacterSelectManager.AvailableChars.Remove(character.ToString());
+                ToggleSelect();
+            }
+        }
+    }
+
+    public string CurrentCharacter()
+    {
+        return character.ToString();
+    }
+}
