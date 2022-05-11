f91eeb5c02879e06baf52bf2ef20acf7ee2c41ce
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-05-11 11:42:33 -0700

    Level Navigation
    - created a level cleared scene
    - locked levels beyond level 1 if you're not in dev mode
    - added dev mode toggle to level select

--- Cursed Crops/Assets/Scripts/UIUX/LevelSelectManager.cs
+++ Cursed Crops/Assets/Scripts/UIUX/LevelSelectManager.cs
@@ -2,10 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelSelectManager : MonoBehaviour
 {
     public GameObject Shop;
+    public int LevelsCleared;
+    public bool DevMode = false;
+
+    // array of selectable/unselectable levels beyond tutorial and lvl1
+    public Button[] LevelButtons = new Button[6];
+
+    private void Start()
+    {
+        // disabling buttons initially
+        foreach (Button B in LevelButtons)
+        {
+            B.interactable = false;
+        }
+
+        // fetch levelsCleared and enable buttons
+        LevelsCleared = PlayerPrefs.GetInt("LevelsCleared");
+        for(int i = 0; i < LevelsCleared - 1; i++) // lvl 1 is not counted here, so -1
+        {
+            LevelButtons[i].interactable = true;
+        }
+    }
+
     public void loadLevel(string level)
     {
         SceneManager.LoadScene(level);
@@ -20,4 +43,30 @@ public class LevelSelectManager : MonoBehaviour
     {
         Shop.SetActive(!Shop.activeSelf);
     }    
+
+    public void ToggleDevMode()
+    {
+        DevMode = !DevMode;
+        PlayerPrefs.SetInt("DevMode", DevMode == true ? 1 : 0);
+        if (DevMode)
+        {
+            foreach (Button B in LevelButtons)
+            {
+                B.interactable = true;
+            }
+        } else
+        {
+            foreach (Button B in LevelButtons)
+            {
+                B.interactable = false;
+            }
+
+            LevelsCleared = PlayerPrefs.GetInt("LevelsCleared");
+            for (int i = 0; i < LevelsCleared - 1; i++) // lvl 1 is not counted here, so -1
+            {
+                LevelButtons[i].interactable = true;
+            }
+        }
+    }
+
 }
