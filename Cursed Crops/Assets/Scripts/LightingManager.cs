94eaacc90b5451657df58a93e4788e87d3c433b0
Daebreak-II <70728190+Daebreak-II@users.noreply.github.com>
2022-05-30 19:49:35 -0700

    Fuck Quaternions (lighting changes)
    - Changed the overhead light to use both x and y axis, as to use shadows to better emulate the sun overhead

--- Cursed Crops/Assets/Scripts/LightingManager.cs
+++ Cursed Crops/Assets/Scripts/LightingManager.cs
@@ -8,15 +8,27 @@ public class LightingManager : MonoBehaviour
     public Color AfternoonLight;
     public Color NightLight;
 
+    public float MorningAngle = 10;
+    public float AfternoonAngle = 70;
+    public float NightAngle = 10;
+
+    public float MorningOffest = 100;
+    public float AfternoonOffset = 180;
+    public float NightOffset = 260;
+
+    public float PhaseTime = 10;
+
     private float time = 0;
     private float AngleTimer = 0;
     private int LightPhase = 1;
+    private int AnglePhase = 1;
     private Quaternion StartRotation;
 
     public Light LightSource;
 
     void Start()
     {
+        this.gameObject.transform.rotation = Quaternion.Euler(MorningAngle, MorningOffest, 0);
         this.StartRotation = this.gameObject.transform.rotation;
     }
 
@@ -26,18 +38,27 @@ public class LightingManager : MonoBehaviour
         switch (LightPhase)
         {
             case 1: 
-                ColorFade(MorningLight, AfternoonLight, 10);
+                ColorFade(MorningLight, AfternoonLight, PhaseTime);
                 break;
             case 2:
-                ColorFade(AfternoonLight, NightLight, 10);
+                ColorFade(AfternoonLight, NightLight, PhaseTime);
                 break;
         }
-        AngleFade(0, 90, 20); 
-    }
 
-    public void ResetTimer()
-    {
-        time = 0;
+        switch (AnglePhase) {
+            case 1:
+                AngleFade(0, AfternoonAngle - MorningAngle,
+                    0, AfternoonOffset - MorningOffest,
+                    PhaseTime);
+                break;
+
+            case 2:
+                AngleFade(AfternoonAngle - MorningAngle, 0,
+                    AfternoonOffset - MorningOffest, NightOffset - MorningOffest,
+                    PhaseTime);
+                break;
+        }
+        // AngleFade(0, 120, 20); 
     }
 
     public void ColorFade(Color startC, Color endC, float t) {
@@ -48,19 +69,27 @@ public class LightingManager : MonoBehaviour
             // Debug.Log("Changing Light: " + time);
         } else
         {
-            LightPhase += 1;
-            ResetTimer();
+            LightPhase++;
+            time = 0;
         }
     }
 
-    public void AngleFade(float startA, float endA, float t)
+    // lerps the lights X angle from start to b, Y angle from b to c, over t (time), 
+    public void AngleFade(float startX, float endX, float startY, float endY, float t)
     {
         if (AngleTimer < 1)
         {
             AngleTimer += Time.deltaTime / t;
-            this.gameObject.transform.rotation = StartRotation * Quaternion.Euler(0, Mathf.Lerp(startA, endA, AngleTimer), 0);
-            Debug.Log("Changing Agnle: " + AngleTimer);
-        }
+            this.gameObject.transform.rotation = Quaternion.Euler(MorningAngle + Mathf.Lerp(startX, endX, AngleTimer),
+                                                                  MorningOffest + Mathf.Lerp(startY, endY, AngleTimer),
+                                                                  0);
 
+            // Debug.Log("Changing X Angle: " + AngleTimer + " by " + Mathf.Lerp(startX, endX, AngleTimer) + " degrees; " + startX + ", " + endX);
+            // Debug.Log("Changing Y Angle: " + AngleTimer + " by " + Mathf.Lerp(startY, endY, AngleTimer) + " degrees; " + startY + ", " + endY);
+        } else
+        {
+            AnglePhase++;
+            AngleTimer = 0;
+        }
     }
 }
